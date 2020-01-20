80b40452fedc17bbadfa8cc7371c791b6ef6a092
Jack Bridger <jacksbridger@gmail.com>
2020-01-20 10:51:08 +0000

    rever changes

--- client/src/index.js
+++ client/src/index.js
@@ -23,7 +23,7 @@ const httpLink = new HttpLink({
 const wsLink = new WebSocketLink({
     uri: 'ws://localhost:4000/',
     options: {
-        reconnect: true
+        // reconnect: true
     }
 });
 
